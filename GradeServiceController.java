466fa16033f1c09e2aa54d7cbcfb1145f5b1426c
frowundas-lang <fro.wundas@gmail.com>
2025-11-02 17:54:13 -0500

    Create GradeServiceController.java

---
+++ GradeServiceController.java
@@ -0,0 +1,7 @@
+package com.university.gradingmicroservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+// ... (rest of the code from the previous response)
