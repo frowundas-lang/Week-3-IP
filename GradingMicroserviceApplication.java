2bf387de43fc07d79832b692b63249ef07e82152
frowundas-lang <fro.wundas@gmail.com>
2025-11-02 17:36:36 -0500

    Add files via upload

---
+++ GradingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.university.grading_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingMicroserviceApplication.class, args);
+	}
+
+}
